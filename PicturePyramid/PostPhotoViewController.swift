72be48a6d28ac950095b69d6aa13021e487d2e96
Bassil Shama <bassil.shama@gmail.com>
2016-07-22 13:40:55 -0700

    first at this point camera still in testing

---
+++ PicturePyramid/PostPhotoViewController.swift
@@ -0,0 +1,84 @@
+//
+//  PostPhotoViewController.swift
+//  PicturePyramid
+//
+//  Created by Bassil Shama on 7/21/16.
+//  Copyright © 2016 Bassil Shama. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PostPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+
+    @IBOutlet weak var cameraView: UIView!
+    
+    @IBAction func unwindToPostPhotoViewController(segue: UIStoryboardSegue) {
+        
+        
+    }
+    
+    var captureSession : AVCaptureSession?
+    var stillImageOutput : AVCaptureStillImageOutput?
+    var previewLayer : AVCaptureVideoPreviewLayer?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        previewLayer?.frame = cameraView.bounds
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBar.hidden = true
+        super.viewWillAppear(animated)
+        
+        captureSession = AVCaptureSession()
+        captureSession?.sessionPreset = AVCaptureSessionPreset1920x1080
+        
+        let backCamera = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        var error : NSError?
+        var input: AVCaptureDeviceInput!
+        do {
+            input = try AVCaptureDeviceInput(device: backCamera)
+        } catch let error1 as NSError {
+            error = error1
+            input = nil
+        }
+        
+        if (error == nil && captureSession?.canAddInput(input) != nil){
+            
+            captureSession?.addInput(input)
+            
+            stillImageOutput = AVCaptureStillImageOutput()
+            stillImageOutput?.outputSettings = [AVVideoCodecKey : AVVideoCodecJPEG]
+            
+            if (captureSession?.canAddOutput(stillImageOutput) != nil){
+                captureSession?.addOutput(stillImageOutput)
+                
+                previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+                previewLayer?.videoGravity = AVLayerVideoGravityResizeAspect
+                previewLayer?.connection.videoOrientation = AVCaptureVideoOrientation.Portrait
+                cameraView.layer.addSublayer(previewLayer!)
+                captureSession?.startRunning()
+                
+            }
+            
+            
+        }
+        
+    }
+
+
+
+}
---
+++ Pods/Target Support Files/Pods-PicturePyramid/Pods-PicturePyramid-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PicturePyramid : NSObject
+@end
+@implementation PodsDummy_Pods_PicturePyramid
+@end
